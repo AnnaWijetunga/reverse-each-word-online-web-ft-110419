d240a3ee7f29147f93e69a8b25a0f93668024ca8
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-07 19:23:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,8 @@ def reverse_each_word(array)
   return reversed_array.join(" ")
 end
 
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
+end
